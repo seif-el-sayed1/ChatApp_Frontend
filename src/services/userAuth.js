10ee2eab0d20651d8cc1b5aa9b856b7018ec97bf
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-10 12:09:21 +0200

    update user auth route

--- src/services/userAuth.js
+++ src/services/userAuth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 
-const USER_AUTH_ROUTE = BASE_URL + "/users/api/v1"
+const USER_AUTH_ROUTE = BASE_URL + "/users/auth"
 
 const api = axios.create({
     baseURL: USER_AUTH_ROUTE, 
