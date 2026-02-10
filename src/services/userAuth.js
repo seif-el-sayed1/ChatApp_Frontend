a8c5982f22c1362bfe74e16aec3ebaf9b658a597
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-10 11:56:52 +0200

    add verify otp/resend otp api calls

--- src/services/userAuth.js
+++ src/services/userAuth.js
@@ -24,3 +24,20 @@ export const registerUser = async (userData) => {
     const response = await api.post("/register", userData);
     return response.data;
 };
+
+// Verify OTP API
+export const verifyOtp = async (email, code) => {
+    const response = await api.post("/verify-account", {
+        email,
+        code
+    });
+    return response.data;
+};
+
+// Resend OTP API
+export const resendOtp = async (email) => {
+    const response = await api.post("/send-otp", {
+        email
+    });
+    return response.data;
+};
