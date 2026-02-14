8693c4dfb7f6efb03959a5283d2f33b07f7e8a17
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-14 11:04:58 +0200

    constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const BASE_URL =  import.meta.env.VITE_BACKEND_URL
+// Backend API URL
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const BASE_URL = import.meta.env.VITE_BASE_URL
