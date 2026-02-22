634cb9d3c9fc1771967483b8e2cda94f95b3dbc4
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-22 03:55:00 +0200

    refactor main.jsx to wrap App component with BrowserRouter and context providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import { UserAuthProvider } from './context/userAuthContext.jsx'
+import { ChatProvider } from './context/chatContext.jsx'
+import { SocketProvider } from './context/socketContext';
+
+const token = localStorage.getItem("token");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserAuthProvider>
+        <SocketProvider token={token}>
+          <ChatProvider>
+            <App />
+          </ChatProvider>
+        </SocketProvider>
+      </UserAuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
